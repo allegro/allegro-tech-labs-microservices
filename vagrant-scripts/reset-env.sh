fae3f3667ac9b689e30845d5c4dcb0bb377ace41
Adam Dubiel <adam.dubiel@allegro.pl>
2018-04-14 08:41:54 +0200

    Sets reset_env.sh file to use /home/ubuntu

--- vagrant-scripts/reset-env.sh
+++ vagrant-scripts/reset-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo service toxiproxy restart
-sudo ./toxiproxy_setup_proxies.sh
+sudo /home/ubuntu/toxiproxy_setup_proxies.sh
 sudo supervisorctl restart data-server
 sudo supervisorctl restart main-service
